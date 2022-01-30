2335c43c9c7574ae90cd2020236bf65f1c827b47
haoyuan336 <328387458@qq.com>
2022-01-30 18:02:51 +0800

    new

--- ExpressServer/routes/msgManager.js
+++ ExpressServer/routes/msgManager.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var messageList = [];
 /* GET home page. */
 router.post('/', function (req, res, next) {
     //   res.render('index', { title: 'Express' });
-    console.log("req"+ req.body);
+    console.log("req" + req.body);
     res.send("success");
+    messageList.push(req.body);
+    console.log("message list = " + messageList);
+});
+router.get('/getNewMsg', function (req, res, next) {
+    if (messageList.length > 0) {
+        var msg = messageList.shift();
+        res.send(msg);
+    }else{
+        res.send("{}");
+    }
 });
 
 module.exports = router;
